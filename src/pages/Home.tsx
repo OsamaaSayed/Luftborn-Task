7d7c04fbb33e8f3c3efe9d3db9cd14f6498d0859
Osama Sayed <osamasayedmuhammad@gmail.com>
2024-02-29 01:51:41 +0200

    add utility classes & implement the aside nav

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,49 @@
 const Home = () => {
-  return <h1>Luftborn - Task 📝</h1>;
+  return (
+    <main className='u-min-h-screen u-container u-mx-auto u-flex u-justify-center u-items-center'>
+      <div className='u-h-full u-flex u-gap-33'>
+        <aside className='aside'>
+          <div className='aside__header'>
+            <img
+              className='aside__header-img u-mx-auto'
+              src='/images/admin.png'
+              alt='admin'
+            />
+
+            <div className='aside__header-title'>
+              <span>Report for</span>
+              <p>Best Sales</p>
+            </div>
+          </div>
+
+          <div className='aside__links'>
+            <a
+              className='aside__links-anchor'
+              href='#'
+            >
+              Daily
+            </a>
+            <a
+              className='aside__links-anchor'
+              href='#'
+            >
+              Weekly
+            </a>
+            <a
+              className='aside__links-anchor'
+              href='#'
+            >
+              Monthly
+            </a>
+          </div>
+        </aside>
+
+        <section>
+          <div className='section-main'></div>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
