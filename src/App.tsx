fbd1274860c5807ea8ed1f2e3e79cb3d9fb16a6d
Osama Sayed <osamasayedmuhammad@gmail.com>
2024-02-28 22:34:34 +0200

    🎉 INIT COMMIT

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from './pages/Home';
+
+import './styles/sass/style.scss';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Luftborn - Task 📝</h1>;
+};
+
+export default Home;
