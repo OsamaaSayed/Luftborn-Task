22d262ad40d2a5d9de960c78ff46cc09e3b0e663
Osama Sayed <osamasayedmuhammad@gmail.com>
2024-03-01 00:59:50 +0200

    finish responsive design for mobile

--- src/components/shared/ProductCard.tsx
+++ src/components/shared/ProductCard.tsx
@@ -15,24 +15,32 @@ const ProductCard = ({
       style={{ backgroundColor }}
     >
       <div className='card__face'>
-        <div className='u-flex u-justify-between'>
-          <h3 className='card__face-category'>{category}</h3>
+        <div className='card__face-header'>
+          <h3 className='category'>{category}</h3>
 
-          <button className='card__face-btn u-flex u-items-center u-gap-3'>
+          <button className='btn'>
             <span className='dot' />
             <span className='dot' />
             <span className='dot' />
           </button>
         </div>
 
+        <div className='card__face-price-sm'>
+          <p className='price'>{price}$</p>
+
+          <span className='title'>
+            {title.length > 35 ? title.slice(0, 35) + ' ...' : title}
+          </span>
+        </div>
+
         <p className='card__face-price'>{price}$</p>
 
-        <div className='u-flex u-justify-between u-gap-22'>
-          <span className='card__face-title'>
+        <div className='card__face-footer'>
+          <span className='title'>
             {title.length > 35 ? title.slice(0, 35) + ' ...' : title}
           </span>
 
-          <div className='u-text-center u-flex u-flex-col gap-3'>
+          <div className='store'>
             <span>InStore</span>
             <span>{rating.count}</span>
           </div>
